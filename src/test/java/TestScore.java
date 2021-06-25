78b84e3160a2e33558ac2c61a7178495d7830a40
Gabriel Jospin <gabriel.jospin@usp.br>
2021-06-24 23:32:59 -0300

    implemeta Test Score

--- src/test/java/TestScore.java
+++ src/test/java/TestScore.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestScore {
+
+    @Test
+    public void test(){
+        Score score = new Score("player1");
+
+        for(int i = 0; i<100; i++){
+            score.getScore();
+            assertEquals("Erro no incremento "+i,i,score.getScore(),0.0);
+            score.inc();
+            score.getScore();
+        }
+
+    }
 }
